28fe685213b0e8f268d8dcf53f12d61ae418045a
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2013-11-13 22:14:47 -0600

    fahr to celsius with for loop 0 to 300

---
+++ fahrcelfor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+  /*fahr to celsius converter 
+    using a for loop */
+
+  int fahr;
+
+
+  for(fahr=0;fahr<=300;fahr = fahr+20)
+    {
+      printf("%3.0d %6.1f\n",fahr, (5.0/9.0)*(fahr - 32.0));
+    }
+}
